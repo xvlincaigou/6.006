85e4640eb2185a54afe77a1221dfdca7aa8770b2
许霖 <l-xu22@mails.tsinghua.edu.cn>
2024-08-22 22:04:16 +0800

    完成ps1

--- ps1/algorithms.py
+++ ps1/algorithms.py
@@ -4,7 +4,11 @@ import trace
 ################################################################################
 ################################## Algorithms ##################################
 ################################################################################
-
+"""
+这个算法是正确的。
+注意到，我们在这个算法中只会考虑某一列的最大的那个元素。而这种策略可以保证，在我们每次的子问题的最外侧的那两列元素中的最大值，一定大于更外面的元素。
+也就是说，这个问题可以轻松转化为我们在课上讲的一维问题。只不过一维里面的所有元素都是我们二维模型中某一列的最大值。
+"""
 def algorithm1(problem, trace = None):
     # if it's empty, we're done 
     if problem.numRow <= 0 or problem.numCol <= 0:
@@ -45,6 +49,16 @@ def algorithm1(problem, trace = None):
     result = algorithm1(sub, trace)
     return problem.getLocationInSelf(sub, result)
 
+"""
+这个算法是对的。
+因为这样的做法最差的情况下会遍历所有元素，而所有元素里面是有一个peak的。
+有趣的是，我一开始没想出来再什么情况下才会是$\theta (mn)$。但是后来找到了。
+1 2 3 4 5 6 7 
+0 0 0 0 0 0 8
+F E D C B A 9
+G 0 0 0 0 0 0
+H I J K L M N
+"""
 def algorithm2(problem, location = (0, 0), trace = None):
     # if it's empty, we're done 
     if problem.numRow <= 0 or problem.numCol <= 0:
@@ -60,6 +74,13 @@ def algorithm2(problem, location = (0, 0), trace = None):
         # there is a better neighbor, so move to the neighbor and recurse
         return algorithm2(problem, nextLocation, trace)
 
+"""
+这个算法是错误的。
+它错误的一种情况已经在problem.py里面写出来了。主要是，当我们在处理一个子条件的时候，我们所使用的bestLoc是在这个子条件里面的，在大多数情况下这没有问题，
+因为如果我们找到的bestLoc在子条件的内部而非边缘，我们可以保证bestSeen永远在我们研究的子条件里面。
+但是如果bestLoc在边缘，那么有可能这个bestLoc在整个条件里面可能不是最大的。
+这个算法可以与4进行比较，其实真正意义上它们只差一个地方，就是在if neighbor == crossLoc:这个地方，4里面加了一个判断，要求这个neighbor的值要大于bestSeen。
+"""
 def algorithm3(problem, bestSeen = None, trace = None):
     # if it's empty, we're done 
     if problem.numRow <= 0 or problem.numCol <= 0:
@@ -129,7 +150,7 @@ def algorithm4(problem, bestSeen = None, rowSplit = True, trace = None):
         subproblems.append((subStartR1, subStartC, subNumR1, subNumC))
         subproblems.append((subStartR2, subStartC, subNumR2, subNumC))
 
-        # get a list of all locations in the dividing column
+        # get a list of all locations in the dividing row
         divider = crossProduct([mid], range(problem.numCol))
     else:
         # the recursive subproblem will involve half the number of columns
